e0ac192ffb59b7bcd459a22a564f23c68846b188
nite48 <nite48ru@gmail.com>
2021-09-23 12:19:15 +0300

    Разделение функционала  валидации и создания карты

---
+++ scripts/Card.js
@@ -0,0 +1,72 @@
+import {openPopup, formImageInsert, formElementContent, formClickImage} from './index.js';
+
+
+export class Card{
+  constructor(data, cardSelector) {
+    this._name = data.name;
+    this._link = data.link;
+    this._cardSelector = cardSelector;
+    console.log(this._cardSelector)
+
+  }
+
+
+  _getTemplate(){
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content
+      .querySelector('.element')
+      .cloneNode(true);
+    return cardElement;
+  }
+
+
+  generateCard(){
+    this._element = this._getTemplate();
+    this._image = this._element.querySelector('.element__image');
+    this._likeButton= this._element.querySelector('.element__heart');
+    this._removeButton = this._element.querySelector('.element__remove-button');
+    this._element.querySelector('.element__title').textContent = this._name;
+    this._image.src = this._link;
+    this._image.alt = this._name;
+    this._setEventListeners();
+    return this._element
+  }
+
+
+  _likeIt(){
+    this._likeButton.classList.toggle('element__heart-black')
+  }
+ 
+
+  _onOpenImage(){
+    openPopup(formClickImage)
+  }
+
+
+  _removeImage(event){
+    this._element.remove();
+  }
+
+
+  _enlargingImage(){
+    const title = event.target.getAttribute('alt');
+    formImageInsert.src = this._link;
+    formImageInsert.alt = this._name;
+    formElementContent.textContent = this._name;
+    this._onOpenImage();
+  }
+ 
+  _setEventListeners(){
+    this._element.querySelector('.element__heart').addEventListener('click', () => {
+      this._likeIt();
+    });
+    this._element.querySelector('.element__image').addEventListener('click', () =>{
+      this._enlargingImage();
+    });
+    this._element.querySelector('.element__remove-button').addEventListener('click', () =>{
+      this._removeImage(event);
+    });
+  }
+}
+
---
+++ scripts/FormValidator.js
@@ -0,0 +1,57 @@
+export class FormValidator{
+  constructor(formElement, parametersValidation){
+    this._formElement = formElement;
+    this._parametersValidation = parametersValidation;
+  }
+  enableValidation(){
+    this._setEventListeners();
+  }
+  _showInputError(inputElement, errorMessage){
+    const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+    //console.log(errorMessage)
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(this._parametersValidation.errorClass);
+    inputElement.classList.add(this._parametersValidation.inputErrorClass);
+  }
+  _hideInputError(inputElement){
+    const errorElement = this._formElement.querySelector(`#${inputElement.id}-error`);
+  
+    errorElement.classList.remove(this._parametersValidation.errorClass);
+    errorElement.textContent = '';
+
+    inputElement.classList.remove(this._parametersValidation.inputErrorClass);
+  }
+  _resolveButton(inputList, buttonElement){
+    const array = inputList[0].value;
+    if (inputList.every((temp) => temp.validity.valid)) {
+      buttonElement.classList.remove(this._parametersValidation.inactiveButtonClass);
+      buttonElement.removeAttribute('disabled');
+      return;
+    }
+
+    buttonElement.classList.add(this._parametersValidation.inactiveButtonClass);
+    buttonElement.setAttribute('disabled', true);
+  }
+  _validateInputField(inputElement){
+    inputElement.validity.valid ? this._hideInputError(inputElement) : this._showInputError(inputElement, inputElement.validationMessage);
+  }
+
+
+  _setEventListeners(){
+    const submitButton = this._formElement.querySelector(this._parametersValidation.submitButtonSelector);
+    const inputFields = Array.from(this._formElement.querySelectorAll(this._parametersValidation.inputSelector));
+    this._resolveButton(inputFields, submitButton);
+    inputFields.forEach(inputElement => {
+      inputElement.addEventListener('input', () => {
+        this._validateInputField(inputElement);
+        this._resolveButton(inputFields, submitButton);
+      });
+    });
+
+    this._formElement.addEventListener('reset', () => {
+      this._resolveButton(inputFields, submitButton);
+    });
+    }
+}
+
+
