01f0ef82881259aeac84f728d8b368c935745332
nite48 <nite48ru@gmail.com>
2021-10-08 16:47:53 +0300

    Добавлен метод PATCH, добавлено отображение лайков

--- src/scripts/Card.js
+++ src/scripts/Card.js
@@ -5,8 +5,13 @@ export default class Card{
   constructor({data, handleCardClick}, cardSelector) {
     this._name = data.name;
     this._link = data.link;
+    this._likes = data.likes;
     this._cardSelector = cardSelector;
     this._handleCardClick = handleCardClick;
+    this._isLiked = false;
+    // console.log(data);
+    
+
 
   }
 
@@ -17,6 +22,7 @@ export default class Card{
       .content
       .querySelector('.element')
       .cloneNode(true);
+    //console.log(cardElement)
     return cardElement;
   }
 
@@ -27,9 +33,14 @@ export default class Card{
     this._likeButton= this._element.querySelector('.element__heart');
     this._removeButton = this._element.querySelector('.element__remove-button');
     this._element.querySelector('.element__title').textContent = this._name;
+    this._countLike = this._element.querySelector('.element__heart-count');
+    console.log(this._countLike.textContent)
+    this._countLike.textContent = this._likes.length;
+    console.log(this._countLike.textContent)
     this._image.src = this._link;
     this._image.alt = this._name;
     this._setEventListeners();
+    //console.log(this._element)
     return this._element
   }
 
@@ -43,6 +54,10 @@ export default class Card{
   _removeImage(event){
     this._element.remove();
   }
+  _count(){
+    this._isLiked = !this._isLiked
+    
+  }
   
 
   _enlargingImage(){
@@ -52,13 +67,15 @@ export default class Card{
   _setEventListeners(){
     this._element.querySelector('.element__heart').addEventListener('click', () => {
       this._likeIt();
+      this._count();
+      
     });
     this._element.querySelector('.element__image').addEventListener('click', () =>{
       this._enlargingImage();
     });
     this._element.querySelector('.element__remove-button').addEventListener('click', () =>{
-      popupRemoveButtonForm.classList.add('popup_activated');
-      // this._removeImage();
+      //popupRemoveButtonForm.classList.add('popup_activated');
+      this._removeImage();
     });
   }
 }
--- src/scripts/UserInfo.js
+++ src/scripts/UserInfo.js
@@ -1,9 +1,13 @@
 export default class UserInfo{
     constructor(userSelector){
+        // console.log(userSelector)
         this._nameProfile = userSelector.nameProfile;
         this._descriptionProfile = userSelector.descriptionProfile;
+        this._avatarProfile = userSelector.avatarProfile;
         this._currentNameProfile = document.querySelector(this._nameProfile);
         this._currentDescriptionProfile = document.querySelector(this._descriptionProfile);
+        // this._currentAvatarProfile = document.querySelector(this._avatarProfile);
+        this._avatar = document.querySelector('.profile__avatar');
         
 
     }
@@ -11,8 +15,11 @@ export default class UserInfo{
       this._objectInfo = {name: this._currentNameProfile.textContent, description: this._currentDescriptionProfile.textContent};
       return this._objectInfo;
     }
-    setUserInfo(name, description){
+    setUserInfo(name, description, avatar, id){
       this._currentNameProfile.textContent = name;
       this._currentDescriptionProfile.textContent = description;
+      this._avatar.src = avatar;
+      //console.log(this._avatar)
+      this._id = id;
     }
 }
